cf7f3d6350940704a4434d693932ecd710a76448
qwerty <euotao@gmail.com>
2022-10-15 17:00:10 +0300

    [Server]: Add Address and CachedTransaction models

--- src/web_bitcoin_client/creator/models.py
+++ src/web_bitcoin_client/creator/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    name = models.CharField(max_length=80)
+    string = models.CharField(max_length=80)
+    type = models.CharField(max_length=30)
+    public = models.CharField(max_length=80)
+    private = models.CharField(max_length=80)
+
+
+class CachedTransaction(models.Model):
+    raw = models.TextField()
+    txid = models.CharField(max_length=100)
+    inps_count = models.IntegerField()
+    outs_count = models.IntegerField()
