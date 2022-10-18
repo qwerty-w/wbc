0c15a69c7a52d573ca0a396c8c111f5b06c814c5
qwerty <itsqwz@gmail.com>
2022-10-18 22:48:46 +0300

    [Server]: Add CachedInput and CachedOutput, refactor CachedTransaction fields

--- src/web_bitcoin_client/creator/models.py
+++ src/web_bitcoin_client/creator/models.py
@@ -2,15 +2,35 @@ from django.db import models
 
 
 class Address(models.Model):
+    string = models.CharField(max_length=80, primary_key=True)
     name = models.CharField(max_length=80)
-    string = models.CharField(max_length=80)
     type = models.CharField(max_length=30)
     public = models.CharField(max_length=80)
     private = models.CharField(max_length=80)
 
 
+class CachedInput(models.Model):
+    string = models.CharField(max_length=80, primary_key=True)
+    type = models.CharField(max_length=30)
+
+
+class CachedOutput(models.Model):
+    string = models.CharField(max_length=80, primary_key=True)
+    type = models.CharField(max_length=30)
+
+
 class CachedTransaction(models.Model):
+    txid = models.CharField(max_length=100, primary_key=True)
+    inputs = models.ManyToManyField(CachedInput)
+    output_address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    other_outputs = models.ManyToManyField(CachedOutput)
+    inputs_amount = models.FloatField()
+    outputs_amount = models.FloatField()
+    full_amount = models.FloatField()
+    fee = models.FloatField()
+    inputs_count = models.IntegerField()
+    outputs_count = models.IntegerField()
+    confirmations = models.IntegerField()
+    size = models.IntegerField()
+    weight = models.IntegerField()
     raw = models.TextField()
-    txid = models.CharField(max_length=100)
-    inps_count = models.IntegerField()
-    outs_count = models.IntegerField()
