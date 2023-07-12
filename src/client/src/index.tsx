7e64eaac1eee209011d48368b5fa80e55e369f3b
qwerty <euotao@gmail.com>
2023-07-12 22:10:04 +0300

    [Client]: Add React frontend, move backend to server (web_btc_client -> server) folder

---
+++ src/client/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css'
+
+import Create from './components/screens/create/Create'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Create />
+  </React.StrictMode>
+);
