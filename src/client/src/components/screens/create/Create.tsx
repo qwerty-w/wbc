0cf228a9e59c8396661b4354866194679ea1d584
qwerty <euotao@gmail.com>
2023-09-02 18:08:15 +0300

    [Client]: Add "new address (create/import)" modal window

--- src/client/src/components/screens/create/Create.tsx
+++ src/client/src/components/screens/create/Create.tsx
@@ -5,20 +5,25 @@ import { Addresses } from './add/Addresses'
 import { Transactions } from './txs/Transactions'
 import { Creator } from './crt/Creator'
 
+import { NewAddress } from './new-address/NewAddress'
+
 import { TransactionProvider } from './txs/context'
 import { IOProvider } from './crt/context'
 
 function Create() {
+    const [NewAddressIsVisible, setNewAddressVisibility] = useState<boolean>(false)
     return (
-    <TransactionProvider>
-        <IOProvider>
-            <div className='main'>
-                <Addresses />
-                <Transactions />
-                <Creator />
-            </div>
-        </IOProvider>
-    </TransactionProvider>
+        <TransactionProvider>
+            <IOProvider>
+                <div className='main'>
+                    <button onClick={() => setNewAddressVisibility(true)}>Change new <br />address visible</button>
+                    <Addresses />
+                    <Transactions />
+                    <Creator />
+                    {NewAddressIsVisible && <NewAddress setVisibility={setNewAddressVisibility}/>}
+                </div>
+            </IOProvider>
+        </TransactionProvider>
     );
 }
 
