529f0e918535103d6696e23e319b8436a2fe2afe
qwerty <euotao@gmail.com>
2023-09-02 22:23:54 +0300

    [Client]: Add context menu to Addresses

--- src/client/src/components/screens/create/add/Addresses.tsx
+++ src/client/src/components/screens/create/add/Addresses.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import './Addresses.css'
+import { useState } from 'react'
+import { INewAddressState } from '../new-address/NewAddress'
+import { onContextMenu, ContextMenu, ContextMenuDivider, ContextMenuItem, IContextMenuPos } from '../../common/context-menu/contextmenu'
 
 const { default: ArrowSvg } = require('./icons/arrow.svg') as { default: string }
 const { default: AddressIcon } = require('./icons/butterfly.svg') as { default: string }
 
-export interface IAddress {
+export { type IAddress, Address, Addresses }
+
+interface IAddress {
     str: string,
     name: string
 }
 
-export function Address({ str, name }: IAddress) {
+function Address({ str, name }: IAddress) {
     return (
         <div className="address">
             <div className="address__left">
@@ -34,13 +38,24 @@ function getAddresses(): IAddress[] {  // TODO
     ]
 }
 
-export function Addresses() {
+interface IAddressesProps {
+    setNewAddressVisibility: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function Addresses({ setNewAddressVisibility }: IAddressesProps) {
     const addresses: IAddress[] = getAddresses()
+    const [contextMenuIsShowed, setContextMenuIsShowed] = useState<boolean>(false)
+    const [contextMenuPos, setContextMenuPos] = useState<IContextMenuPos>({top: 0, left: 0})
+    const contextMenuState = { isShowed: contextMenuIsShowed, setIsShowed: setContextMenuIsShowed, pos: contextMenuPos, setPos: setContextMenuPos }
+
     return (
-        <div className='add'>
+        <div className='add' onContextMenu={onContextMenu(contextMenuState)}>
             {
                 addresses.map((address, index) => <Address key={index} str={address.str} name={address.name} />)
             }
+            <ContextMenu state={contextMenuState}>
+                <ContextMenuItem name='Add new address' onClick={ ev => { setNewAddressVisibility(true) } } />
+            </ContextMenu>
         </div>
     )
 }
