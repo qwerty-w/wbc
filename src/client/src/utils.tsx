3515d2029e305bd666ea57c7e514d652afde40ce
qwerty <euotao@gmail.com>
2023-09-09 03:42:39 +0300

    [Client]: Add FiltredInput, input for BTC amount

--- src/client/src/utils.tsx
+++ src/client/src/utils.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from "react"
 
 
 export function wrapString(str: string, letters: number = 4) {
@@ -17,4 +18,66 @@ export function toBitcoins(num: number) {
 
 export async function setBuffer(text: string) {
     await navigator.clipboard.writeText(text)
+}
+
+interface FiltredInputProps {
+    filter: (pos: number, value: string) => { pos: number, value: string },
+    onBlur: (value: string) => string
+    defval?: string
+}
+
+export function FiltredInput({ filter, onBlur, defval = '' }: FiltredInputProps) {
+    const ref = useRef<HTMLInputElement>(null)
+    const [value, setValue] = useState<string>(defval)
+    const [cursorPos, setCursorPos] = useState<number | null>(null)
+    const [wasChanged, setWasChanged] = useState<boolean>(false)
+
+    const toggleChange = () => { setWasChanged(!wasChanged) }
+    
+    useEffect(() => {
+        if (cursorPos === null) { 
+            return
+        }
+
+        ref.current?.setSelectionRange(cursorPos, cursorPos)
+    }, [wasChanged])
+
+    return (
+        <input
+            ref={ref}
+            type="text"
+            value={value}
+            onBlur={ev => { setValue(onBlur(ev.target.value)) }}
+            onChange={ev => {
+                const { pos, value } = filter((ev.target.selectionStart as number), ev.target.value)
+                setValue(value)
+                setCursorPos(pos)
+                toggleChange()
+            }}
+        />
+    )
+}
+
+interface BTCamountInputProps { 
+    defval?: string
+}
+
+export function BTCamountInput({ defval = '' }: BTCamountInputProps) {
+    return <FiltredInput filter={(pos, raw) => { 
+        let value = ''
+
+        for (let char of raw) {
+            if ('0123456789'.includes(char)) {
+                value += char
+            }
+            else if ('.,'.includes(char) && !value.includes('.')) {
+                value += '.'
+            }
+            else { 
+                --pos
+            }
+        }
+
+        return { pos, value }
+     }} onBlur={val => String(Number(val))} defval={defval} />
 }
