236f3136607601bc448e0329c138f57a0653eacd
qwerty <itsqwz@gmail.com>
2023-09-15 13:59:21 +0300

    [Client]: Update FiltredInput state using mobx

--- src/client/src/components/screens/create/Create.tsx
+++ src/client/src/components/screens/create/Create.tsx
@@ -4,8 +4,7 @@ import { createContext } from 'react'
 import { Container } from '../../../utils'
 import { AddressContainer, AddressesView, getAddresses } from './add/Addresses'
 import { Transaction, TransactionsView } from './txs/Transactions'
-import { CreatorState, Creator } from './crt/Creator'
-import { TransactionProvider } from './txs/context'
+import { Creator, CreatorView } from './crt/Creator'
 import { ModalContextProvider } from '../common/modal/modal'
 import { NewAddressModal, NewAddressContext } from './new-address/NewAddress'
 import { NewOutputModal, NewOutputContext } from './new-output/NewOutput'
@@ -21,30 +20,26 @@ export { GlobalStateContext, Create }
 interface GlobalState {
     addrs: AddressContainer,
     txs: Container<Transaction>,
-    creator: CreatorState,
+    creator: Creator,
     // modals: IModalStates
 }
 
-const GlobalStateContext = createContext<GlobalState>({ addrs: new AddressContainer(), txs: new Container('id'), creator: new CreatorState() })
+const GlobalStateContext = createContext<GlobalState>({ addrs: new AddressContainer(), txs: new Container('id'), creator: new Creator() })
 
 const Create = () => {
     return (
-        <>
-        <GlobalStateContext.Provider value={{ addrs: new AddressContainer(getAddresses()), txs: new Container('id'), creator: new CreatorState() }}>
-            <TransactionProvider>
-                <ModalContextProvider context={NewAddressContext}>
-                    <ModalContextProvider context={NewOutputContext}>
-                        <div className='main'>
-                            <AddressesView />
-                            <TransactionsView />
-                            <Creator />
-                            <NewAddressModal />
-                            <NewOutputModal />
-                        </div>
-                    </ModalContextProvider>
+        <GlobalStateContext.Provider value={{ addrs: new AddressContainer(getAddresses()), txs: new Container('id'), creator: new Creator() }}>
+            <ModalContextProvider context={NewAddressContext}>
+                <ModalContextProvider context={NewOutputContext}>
+                    <div className='main'>
+                        <AddressesView />
+                        <TransactionsView />
+                        <CreatorView />
+                        <NewAddressModal />
+                        <NewOutputModal />
+                    </div>
                 </ModalContextProvider>
-            </TransactionProvider>
+            </ModalContextProvider>
         </GlobalStateContext.Provider>
-        </>
     );
 }
--- src/client/src/components/screens/create/new-output/NewOutput.tsx
+++ src/client/src/components/screens/create/new-output/NewOutput.tsx
@@ -1,62 +1,51 @@
 import './NewOutput.css'
-import { createContext, useContext, useState, useRef, useEffect } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { ModalShowType, Modal } from '../../common/modal/modal'
-import { toSatoshis, BTCamountInput, IFiltredInputStateInstance, FiltredInput } from '../../../../utils'
+import { toSatoshis, FiltredInput, FiltredInputView, BTCamountInputView } from '../../../../utils'
 import { GlobalStateContext } from '../Create'
 import { Output } from '../crt/Creator'
+import { observer } from 'mobx-react-lite'
 
 export { NewOutputContext, NewOutputModal }
 
 const NewOutputContext = createContext<ModalShowType>({ isShowed: false, setIsShowed: () => {} })
 
-function NewOutputModal() {
+const NewOutputModal = observer(() => {
     const { isShowed, setIsShowed } = useContext(NewOutputContext)
-    const { outs } = useContext(GlobalStateContext).creator
+    const creator = useContext(GlobalStateContext).creator
+    const { outs } = creator
 
     const [continueDisabled, setContinueDisabled] = useState(true)
-    const [addressState, setAddressState] = useState<IFiltredInputStateInstance>({ currentValue: String(), isInvalid: false })
-    const [amountState, setAmountState] = useState<IFiltredInputStateInstance>({ currentValue: String(), isInvalid: false })
+    
+    const [address] = useState(new FiltredInput((pos, value) => { return { pos, value } }))
+    const [amount] = useState(new FiltredInput((pos, value) => { return { pos, value } }, undefined, '0'))
 
-    useEffect(() => {addressState.currentValue === '' ? setContinueDisabled(true) : setContinueDisabled(false) }, [addressState])
-    useEffect(() => {setAddressState({ ...addressState, isInvalid: false }); setAmountState({ ...amountState, isInvalid: false })}, [isShowed])
+    useEffect(() => { address.value === '' ? setContinueDisabled(true) : setContinueDisabled(false) }, [address.value])
+    useEffect(() => { address.setInvalid(false); amount.setInvalid(false) }, [isShowed])
 
     const onContinue = () => {
-        const address = addressState.currentValue
-        const amount = toSatoshis(Number(amountState.currentValue))
-        var invalid = false
+        const [addr, am] = [address.value, toSatoshis(Number(amount.value))]
+        address.setInvalid(addr === '' || !addr.split('').every(char => '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'.includes(char)))
+        amount.setInvalid(am <= 0 || Number.isNaN(am) || am > creator.remainder)
 
-        if (amount <= 0 || Number.isNaN(amount)) {
-            setAmountState({ ...amountState, isInvalid: true })
-            invalid = true
+        if (address.isInvalid || amount.isInvalid) {
+            return 
         }
-        else {
-            setAmountState({ ...amountState, isInvalid: false })
-        }
-        if (address === '' || !address.split('').every(char => '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'.includes(char))) {
-            setAddressState({ ...addressState, isInvalid: true })
-            invalid = true
-        }
-        else {
-            setAddressState({ ...addressState, isInvalid: false })
-        }
-        if (!invalid) {
-            outs.add(new Output(addressState.currentValue, amount))
-            setIsShowed(false)
-        }
+
+        outs.add(new Output(addr, am))
+        setIsShowed(false)
     }
 
     return (
         <Modal context={NewOutputContext}>
             <div className='new-out'>
                 <div className='new-out__addr new-out__item'>
-                    <FiltredInput filter={(pos, value) => {
-                        return { pos, value }
-                    }} state={{ ins: addressState, set: setAddressState }}/>
+                    <FiltredInputView inp={address} />
                     <span className='new-out__item-label'>Address</span>
                 </div>
                 <div className='new-out__bot'>
                     <div className='new-out__amount new-out__item'>
-                        <BTCamountInput state={{ ins: amountState, set: setAmountState }} defaultValue='0'/>
+                        <BTCamountInputView inp={amount} />
                         <span className='new-out__item-label'>Amount</span>
                     </div>
                     <button className={`new-out__continue${continueDisabled ? ' disabled' : ''}`} disabled={continueDisabled} onClick={onContinue}>Continue</button>
@@ -64,4 +53,4 @@ function NewOutputModal() {
             </div>
         </Modal>
     )
-}
+})
