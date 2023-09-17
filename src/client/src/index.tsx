c2c335d961306ef80d4fb0ce5ec60acca75f83ad
qwerty <euotao@gmail.com>
2023-09-17 15:26:43 +0300

    [Client]: Add routing

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,11 +1,24 @@
-import ReactDOM from 'react-dom/client';
 import './global.css'
 
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Create } from './components/screens/create/Create'
 
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement);
 
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Create />}/>
+                <Route path='/address/:addr' element={<Create />}/>
+                <Route path='/transaction/:tx' element={<Create />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
 root.render(
-    <Create />
+    <App />
 );
