7d941bae98bff33293fa8f4532e5ec17aa64e2b2
qwerty <itsqwz@gmail.com>
2023-11-06 16:47:36 +0300

    [Client]: Popup bug fixes, refactoring, move transition to separate file
    - Add timer stopping after item remove
    - Add single point of item removing without animation - popup.items.remove()
    - Prevent peinding.ondel overflow (pending.ondel > popup.items.length)
    - Move timer to separate component
    - Move out popup transitions (add/clearing) from useEffect, cause popup
      is first rendered in DEFAULT state, and only then (in useEffect) the
      animation starts, which can be seen on weak devices (item will appear
      first (DEFAULT) and then animation will only begin (ENTER))
    - useLayoutEffect() will be called only when popup gets new item (reduce
      the load)

---
+++ src/transition.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, ReactElement } from 'react'
+import { observable, action, makeObservable } from 'mobx'
+import { observer } from 'mobx-react-lite'
+
+export enum TransitionState {
+    DEFAULT = 'DEFAULT',
+    ENTER = 'ENTER',
+    ENTERING = 'ENTERING',
+    ENTERED = 'ENTERED'
+}
+
+export class Transition {
+    public state: TransitionState = TransitionState.DEFAULT
+    public started: boolean = false
+
+    constructor() {
+        makeObservable(this, {
+            started: observable,
+            state: observable,
+            start: action,
+            reset: action,
+            setState: action
+        })
+    }
+    start() {
+        if (this.started) return
+        this.started = true
+        this.state = TransitionState.ENTER
+    }
+    reset() {
+        this.started = false
+        this.state = TransitionState.DEFAULT
+    }
+    setState(state: TransitionState) {
+        this.state = state
+    }
+}
+
+type TransitionProps = {
+    transition: Transition,
+    timeout: number,
+    callback: (state: TransitionState) => ReactElement
+}
+
+export const TransitionView = observer(({ transition, timeout, callback }: TransitionProps) => {
+    useEffect(() => {
+        if (transition.started) {
+            setTimeout(() => {
+                transition.setState(TransitionState.ENTERING)
+                setTimeout(() => transition.setState(TransitionState.ENTERED), timeout)
+            }, 10)
+        }
+    }, [transition, transition.started])
+
+    return callback(transition.state)
+})
