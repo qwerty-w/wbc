9903aad3d9ec83f09eba95238e7c6a69e9936a80
qwerty <euotao@gmail.com>
2024-08-02 15:52:36 +0300

    [fastapi]: Add cryptoutils.py

--- src/fastapi/src/auth/views.py
+++ src/fastapi/src/auth/views.py
@@ -4,7 +4,7 @@ from fastapi import Depends, Form, APIRouter, HTTPException, Request, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from ..models import User
-from . import crud, currentuser, schema, models
+from . import crud, currentuser, schema, models, cryptoutils as cu
 
 
 router = APIRouter(prefix='/auth')
@@ -13,18 +13,6 @@ oauth2 = OAuth2PasswordBearer(tokenUrl='/signin')
 
 async def add_usersession(user: User, request: Request):
     return await crud.add_usersession(user.id, getattr(request.client, 'host', None), request.headers.get('User-Agent'))
-
-
-@router.post('/signin')
-async def signin(formdata: Annotated[OAuth2PasswordRequestForm, Depends()], request: Request):
-    u = await crud.get_user_by_username(formdata.username)
-    if not u:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='user not found')
-    if not crud.pwdcontext.verify(formdata.password, u.pwd):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='wrong password')
-    session = await add_usersession(u, request)
-    return schema.UserSession(username=u.username, access_token=session.token, expire=session.expire)
-
 
 @router.post('/signup')
 async def signup(
@@ -38,6 +26,17 @@ async def signup(
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='username already exists')
 
     u = await crud.add_user(username, password)
+    session = await add_usersession(u, request)
+    return schema.UserSession(username=u.username, access_token=session.token, expire=session.expire)
+
+
+@router.post('/signin')
+async def signin(formdata: Annotated[OAuth2PasswordRequestForm, Depends()], request: Request):
+    u = await crud.get_user_by_username(formdata.username)
+    if not u:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='user not found')
+    if not cu.context.verify(formdata.password, u.pwd):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='wrong password')
     session = await add_usersession(u, request)
     return schema.UserSession(username=u.username, access_token=session.token, expire=session.expire)
 
