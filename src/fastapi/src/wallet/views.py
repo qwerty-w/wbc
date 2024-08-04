e86102648230c5d016dd0981612af60791c564c9
qwerty <euotao@gmail.com>
2024-08-04 23:35:08 +0300

    [fastapi]: Add another inputs (wif/base64/hex) types for obtain address

--- src/fastapi/src/wallet/views.py
+++ src/fastapi/src/wallet/views.py
@@ -1,6 +1,7 @@
+import base64
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
-from btclib import PrivateKey
+from btclib import PrivateKey, BaseAddress
 
 from ..models import User
 from ..auth import currentuser
@@ -11,36 +12,49 @@ from . import schema, crud
 router = APIRouter(prefix='/wallet')
 
 
-@router.get('/address/list', response_model=list[schema.AddressOut])
+def pvfrom(a: schema.ObtainedAddressIn) -> schema.AddressInfo:
+    match a.intype:
+        case schema.InputKeyType.wif:
+            try:
+                p = PrivateKey.from_wif(a.input)
+            except (ValueError, AssertionError):
+                raise HTTPException(status.HTTP_400_BAD_REQUEST, 'invalid wif')
+
+            p.public.compressed = a.pubkey_compressed
+            p.public.network = a.network
+
+        case schema.InputKeyType.hex:
+            p = PrivateKey.from_bytes(bytes.fromhex(a.input))
+
+        case schema.InputKeyType.base64:
+            p = PrivateKey.from_bytes(base64.b64decode(a.input.encode()))
+
+    return schema.AddressInfo(p, p.public.get_address(a.type))
+
+
+@router.get('/address/list', response_model=list[schema.UserAddressOut])
 async def addresses(user: Annotated[User, Depends(currentuser)]):
     return await crud.get_addresses(user.id)
 
 
-@router.post('/address/create', response_model=schema.AddressOut, description='Generate private key, get address and save it')
+@router.post('/address/create', response_model=schema.UserAddressOut, description='Generate private key, get address and save it')
 async def create_address(user: Annotated[User, Depends(currentuser)], a: schema.CreateAddressIn):
     try:
-        return await crud.create_address(user, a.userpassword,a.type, a.network, a.shortname, a.emojid, a.pubkey_compressed)
+        return await crud.create_address(user, a.userpassword, a.type, a.network, a.shortname, a.emojid, a.pubkey_compressed)
     except ValueError:
         raise InvalidPasswordError
 
 
 @router.post('/address/import')
-async def import_address():
+async def import_address(user: Annotated[User, Depends(currentuser)], a: schema.ImportAddressIn):
     pass
 
 
 @router.post('/address/obtain', response_model=schema.ObtainedAddressOut, description='Get address from private key WIF')
-async def obtain_address(a: schema.ObtainedAddressIn):
-    try:
-        p = PrivateKey.from_wif(a.pvwif)
-    except (ValueError, AssertionError):
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, 'invalid wif')
-
-    pb = p.public.change_compression(a.pubkey_compressed).change_network(a.network)
-    address = pb.get_address(a.type)
+async def obtain_address(inf: Annotated[schema.AddressInfo, Depends(pvfrom)]):
     return {
-        'string': address.string,
-        'type': address.type,
-        'network': address.network,
-        'pubkey_compressed': pb.compressed
+        'type': inf.address.type,
+        'network': inf.address.network,
+        'pubkey_compressed': inf.p.public.compressed,
+        'string': inf.address.string
     }
