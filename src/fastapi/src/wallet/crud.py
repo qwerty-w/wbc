bba800ef11411ce03897dda6f8a17b3a7642bda1
qwerty <itsqwz@gmail.com>
2024-08-04 19:06:11 +0300

    [fastapi]: Add wallet create_address view

--- src/fastapi/src/wallet/crud.py
+++ src/fastapi/src/wallet/crud.py
@@ -1,9 +1,51 @@
 from sqlalchemy import select
+from btclib import PrivateKey, NetworkType, AddressType
+
 from ..database import SessionLocal
 from ..models import User
-from .models import UserBitcoinAddress
+from . import cryptoutils as cu
+from .models import UserBitcoinKey, UserBitcoinAddress
 
 
 async def get_addresses(userid: int):
     async with SessionLocal() as session:
         return await session.scalars(select(UserBitcoinAddress).where(UserBitcoinAddress.userid == userid))
+
+
+async def create_address(user: User,
+                         userpassword: str,
+                         type: AddressType,
+                         network: NetworkType,
+                         shortname: str,
+                         emojid: str,
+                         pubkey_compressed: bool = True,
+                         p: PrivateKey | None = None) -> UserBitcoinAddress:
+
+    p = p or PrivateKey(pubkey_network=network, pubkey_compressed=pubkey_compressed)
+    rawp, rawpub = p.to_bytes(), p.public.key.to_string()
+    pubx, puby = rawpub[:32], rawpub[32:]
+    address = p.public.get_address(type)
+    ck = cu.kdfdecrypt(userpassword, user.ckey_encrypted, user.kdf_options, user.kdf_digest)
+
+    async with SessionLocal() as session:
+        session.add(bkey := UserBitcoinKey(
+            userid=user.id,
+            encrypted=cu.encrypt(ck, rawp),
+            dsha256_digest=cu.dsha256(rawp),
+            pubkey_xb=pubx,
+            pubkey_yb=puby
+        ))
+        await session.commit()
+
+        session.add(baddr := UserBitcoinAddress(
+            userid=user.id,
+            string=address.string,
+            type=address.type,
+            network=address.network,
+            is_pubkey_compressed=p.public.compressed,
+            keyid=bkey.id,
+            shortname=shortname,
+            emojid=emojid
+        ))
+        await session.commit()
+        return baddr
--- src/fastapi/src/wallet/schema.py
+++ src/fastapi/src/wallet/schema.py
@@ -1,12 +1,21 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+from btclib import NetworkType, AddressType
+
 from .models import UserBitcoinAddress
 
 
-class Address(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
-    string: str
-    type: str
-    network: str
-    shortname: str
+class BaseAddress(BaseModel):
+    type: AddressType
+    network: NetworkType = NetworkType.MAIN
+    shortname: str = Field(max_length=UserBitcoinAddress.shortname.type.length)
     emojid: str
+
+
+class CreateAddressIn(BaseAddress):
+    userpassword: str
+
+
+class AddressOut(BaseAddress):
+    string: str
+
+    model_config = ConfigDict(from_attributes=True)
