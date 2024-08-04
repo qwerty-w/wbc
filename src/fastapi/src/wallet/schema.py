599a3868086c4f29c88a777e3141cb21618a9886
qwerty <itsqwz@gmail.com>
2024-08-04 23:35:08 +0300

    [fastapi]: Add another inputs (wif/base64/hex) types for obtain address

--- src/fastapi/src/wallet/schema.py
+++ src/fastapi/src/wallet/schema.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel, ConfigDict, Field
-from btclib import NetworkType, AddressType
+import re
+from enum import StrEnum
+from typing import ClassVar
+from dataclasses import dataclass
+from pydantic import BaseModel, ConfigDict, Field, field_validator, ValidationInfo
+from btclib import PrivateKey, BaseAddress as BaseBitcoinAddress, NetworkType, AddressType
 
 from .models import UserBitcoinAddress
+
+
+class InputKeyType(StrEnum):
+    wif = 'wif'
+    hex = 'hex'
+    base64 = 'base64'
+
+
+@dataclass
+class AddressInfo:
+    p: PrivateKey
+    address: BaseBitcoinAddress
 
 
 class BaseAddress(BaseModel):
@@ -10,24 +26,44 @@ class BaseAddress(BaseModel):
     pubkey_compressed: bool = True
 
 
+class ObtainedAddressIn(BaseAddress):
+    _vin: ClassVar = {
+        InputKeyType.hex: re.compile(r'[a-fA-F0-9]{64}'),  # or
+        InputKeyType.base64: re.compile(r'[A-Za-z0-9+/]{40}[A-Za-z0-9+/]{3}=')
+    }
+
+    intype: InputKeyType
+    input: str
+
+    @field_validator('input')
+    @classmethod
+    def validate_in(cls, v: str, info: ValidationInfo):
+        for type, regex in cls._vin.items():
+            if info.data['intype'] == type:
+                if not regex.fullmatch(v):
+                    raise ValueError(f"invalid {type.value} string for regex pattern '{regex.pattern}'")
+                break
+        return v
+
+
+class ObtainedAddressOut(BaseAddress):
+    string: str
+
+
 class UserAddress(BaseAddress):
     shortname: str = Field(max_length=UserBitcoinAddress.shortname.type.length)
     emojid: str
+
+
+class UserAddressOut(UserAddress):
+    string: str
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class CreateAddressIn(UserAddress):
     userpassword: str
 
 
-class AddressOut(UserAddress):
-    string: str
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class ObtainedAddressIn(BaseAddress):
-    pvwif: str
-
-
-class ObtainedAddressOut(BaseAddress):
-    string: str
+class ImportAddressIn(CreateAddressIn, ObtainedAddressIn):
+    pass
