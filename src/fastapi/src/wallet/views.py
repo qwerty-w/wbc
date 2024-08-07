9cbaebf88961120e4505dca164612a3066e42b18
qwerty <itsqwz@gmail.com>
2024-08-07 10:19:18 +0300

    [fastapi]: Refactor wallet/views.py

--- src/fastapi/src/wallet/views.py
+++ src/fastapi/src/wallet/views.py
@@ -1,8 +1,8 @@
 import base64
-from typing import Annotated, overload, Literal
+from typing import Annotated
 from fastapi import Request, status, APIRouter, Depends, Path, HTTPException
 
-from btclib import PrivateKey, BaseAddress
+from btclib import PrivateKey
 from ..models import User
 from ..auth import currentuser
 from ..auth.exceptions import InvalidPasswordError
@@ -13,7 +13,7 @@ router = APIRouter(prefix='/wallet')
 
 
 @router.get(
-    '/address/list',
+    '/address',
     response_model=list[schema.UserAddressOut]
 )
 async def get_addresses(user: Annotated[User, Depends(currentuser)]):
@@ -33,8 +33,48 @@ async def currentaddress(
     '/address/{addresstr}',
     response_model=schema.UserAddressOut
 )
-async def get_address(address: Annotated[models.UserBitcoinAddress, Depends(currentaddress)]):
+async def get_address(
+    address: Annotated[models.UserBitcoinAddress, Depends(currentaddress)]
+):
     return address
+
+
+def pvfrom(a: schema.ObtainedAddressIn) -> PrivateKey:
+    match a.intype:
+        case schema.InputKeyType.wif:
+            try:
+                p = PrivateKey.from_wif(a.input)
+            except (ValueError, AssertionError):
+                raise HTTPException(status.HTTP_400_BAD_REQUEST, 'invalid wif')
+
+            p.public.compressed = a.pubkey_compressed
+            p.public.network = a.network
+
+        case schema.InputKeyType.hex:
+            p = PrivateKey.from_bytes(bytes.fromhex(a.input))
+
+        case schema.InputKeyType.base64:
+            p = PrivateKey.from_bytes(base64.b64decode(a.input.encode()))
+
+    return p
+
+
+@router.post(
+    '/address:obtain',
+    response_model=schema.ObtainedAddressOut,
+    description='Get address from private key'
+)
+async def obtain_address(
+    p: Annotated[PrivateKey, Depends(pvfrom)],
+    a: schema.ObtainedAddressIn
+):
+    address = p.public.get_address(a.type)
+    return {
+        'type': address.type,
+        'network': address.network,
+        'pubkey_compressed': p.public.compressed,
+        'string': address.string
+    }
 
 
 async def check_shortname_exists(
@@ -57,65 +97,11 @@ async def check_shortname_exists(
         )
 
 
-@router.put(
-    '/address/{addresstr}',
-    response_model=schema.UserAddressOut,
-    description='Update address params'
-)
-async def put_address(
-    user: Annotated[User, Depends(currentuser)],
-    address: Annotated[models.UserBitcoinAddress, Depends(currentaddress)],
-    params: schema.MutableUserAddressParams
+async def newaddr(
+    user: User,
+    a: schema.CreateAddressIn,
+    p: PrivateKey | None = None
 ):
-    if any(v != getattr(address, n) for n, v in params):
-        await check_shortname_exists(user, shortname=params.shortname)
-        await crud.update_address(user.id, address.string, **params.model_dump())
-        address = await currentaddress(user, address.string)
-    return address
-
-
-@overload
-def pvfrom(a: schema.ObtainedAddressIn, *, withaddress: Literal[True] = ...) -> tuple[PrivateKey, BaseAddress]:
-    ...
-@overload
-def pvfrom(a: schema.ObtainedAddressIn, *, withaddress: Literal[False] = ...) -> tuple[PrivateKey, None]:
-    ...
-def pvfrom(a: schema.ObtainedAddressIn, *, withaddress: bool = True) -> tuple[PrivateKey, BaseAddress | None]:
-    match a.intype:
-        case schema.InputKeyType.wif:
-            try:
-                p = PrivateKey.from_wif(a.input)
-            except (ValueError, AssertionError):
-                raise HTTPException(status.HTTP_400_BAD_REQUEST, 'invalid wif')
-
-            p.public.compressed = a.pubkey_compressed
-            p.public.network = a.network
-
-        case schema.InputKeyType.hex:
-            p = PrivateKey.from_bytes(bytes.fromhex(a.input))
-
-        case schema.InputKeyType.base64:
-            p = PrivateKey.from_bytes(base64.b64decode(a.input.encode()))
-
-    return p, withaddress and p.public.get_address(a.type) or None
-
-
-@router.post(
-    '/address/obtain',
-    response_model=schema.ObtainedAddressOut,
-    description='Get address from private key'
-)
-async def obtain_address(pa: Annotated[tuple[PrivateKey, BaseAddress], Depends(pvfrom)]):
-    p, address = pa
-    return {
-        'type': address.type,
-        'network': address.network,
-        'pubkey_compressed': p.public.compressed,
-        'string': address.string
-    }
-
-
-async def newaddr(user: User, a: schema.CreateAddressIn, p: PrivateKey | None = None):
     try:
         return await crud.create_address(
             user,
@@ -136,7 +122,7 @@ async def newaddr(user: User, a: schema.CreateAddressIn, p: PrivateKey | None = 
 
 
 @router.post(
-    '/address/create',
+    '/address:create',
     response_model=schema.UserAddressOut,
     dependencies=[Depends(check_shortname_exists)],
     description='Generate private key, get address and save it'
@@ -149,13 +135,30 @@ async def create_address(
 
 
 @router.post(
-    '/address/import',
+    '/address:import',
     dependencies=[Depends(check_shortname_exists)],
     response_model=schema.UserAddressOut
 )
 async def import_address(
     user: Annotated[User, Depends(currentuser)],
+    p: Annotated[PrivateKey, Depends(pvfrom)],
     a: schema.ImportAddressIn
 ):
-    p, _ = pvfrom(a, withaddress=False)
     return await newaddr(user, a, p)
+
+
+@router.put(
+    '/address/{addresstr}',
+    response_model=schema.UserAddressOut,
+    description='Update address params'
+)
+async def put_address(
+    user: Annotated[User, Depends(currentuser)],
+    address: Annotated[models.UserBitcoinAddress, Depends(currentaddress)],
+    params: schema.MutableUserAddressParams
+):
+    if any(v != getattr(address, n) for n, v in params):
+        await check_shortname_exists(user, shortname=params.shortname)
+        await crud.update_address(user.id, address.string, **params.model_dump())
+        address = await currentaddress(user, address.string)
+    return address
