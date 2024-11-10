ce1b7e4115cb5b28cecc9256105153ce7ad98ba6
qwerty <euotao@gmail.com>
2024-11-10 21:38:17 +0300

    [react]: Refactor TextInput component

--- src/client/src/core/components/TextInput.tsx
+++ src/client/src/core/components/TextInput.tsx
@@ -2,90 +2,129 @@ import { useState, useRef, useEffect } from "react"
 import { makeObservable, observable, action} from 'mobx'
 import { observer } from "mobx-react-lite"
 import styled from 'styled-components'
+import { isDigitChar, isSingleChar, removeChar } from "../utils/utils"
 
 
 export const StyledTextInput = styled.input<{ $invalid?: boolean }>`
     color: ${ props => props.$invalid ? '#E73838' : 'unset' };
 `
 
-export class FiltredTextInput {
-    value: string
-    cursorPos: number
+interface IFilteredTextInputParams {
+    oninput?: (value: string, cursorPos: number, e: React.ChangeEvent<HTMLInputElement>) => { value: string, cursorPos?: number }
+    onblur?: (e: React.FocusEvent<HTMLInputElement>) => void
+    defvalue?: string
+    invalid?: boolean
+}
 
-    constructor(public filter: (pos: number, value: string) => { pos: number, value: string },
-                public onBlur?: (ev: React.FocusEvent<HTMLInputElement>, input: FiltredTextInput) => void, defaultValue: string = '',
-                public isInvalid: boolean = false) {
-        this.value = defaultValue
+export class FilteredTextInput {
+    public oninput: IFilteredTextInputParams['oninput']
+    public onblur: IFilteredTextInputParams['onblur']
+    public value: string
+    public cursorPos: number
+    public invalid: boolean
+    // need only for useEffect after every onChange() event
+    // (for setSelectionRange)
+    public changed: boolean
+
+    constructor(
+        {
+            oninput,
+            onblur,
+            defvalue,
+            invalid
+        }: IFilteredTextInputParams = {}
+    ) {
+        this.oninput = oninput
+        this.onblur = onblur
+        this.value = defvalue || ''
         this.cursorPos = 0
+        this.invalid = invalid || false
+        this.changed = false
         makeObservable(this, {
             value: observable,
             cursorPos: observable,
-            isInvalid: observable,
+            invalid: observable,
+            changed: observable,
             setValue: action,
             setCursorPos: action,
-            setInvalid: action
+            setInvalid: action,
+            toggleChanged: action
         })
     }
-    setValue(val: string) {
-        this.value = val
+    setValue(v: string) {
+        this.value = v
     }
     setCursorPos(pos: number) {
         this.cursorPos = pos
     }
     setInvalid(val: boolean) {
-        this.isInvalid = val
+        this.invalid = val
+    }
+    toggleChanged() {
+        this.changed = !this.changed
     }
 }
 
-interface IFiltredTextInputProps {
-    inp: FiltredTextInput
+export const useFiltredTextInputRef = (): React.MutableRefObject<FilteredTextInput | undefined> => {
+    return useRef<FilteredTextInput>()
 }
 
-export const FiltredTextInputView = observer(({ inp }: IFiltredTextInputProps) => {
-    const ref = useRef<HTMLInputElement>(null)
-    const [changed, setChanged] = useState<boolean>(false)
+type FilteredTextInputRef = React.MutableRefObject<FilteredTextInput | null | undefined>
 
-    const toggleChange = () => { setChanged(!changed) }
+interface IFilteredTextInputViewProps extends IFilteredTextInputParams {
+    inputRef?: FilteredTextInputRef
+}
+
+export const FilteredTextInputView = observer((props: IFilteredTextInputViewProps) => {
+    const elementRef = useRef<HTMLInputElement>(null)
+    const [input] = useState<FilteredTextInput>(new FilteredTextInput(props))
+    if (props.inputRef && !props.inputRef.current) props.inputRef.current = input
 
     useEffect(() => {
-        ref.current?.setSelectionRange(inp.cursorPos, inp.cursorPos)
-    }, [changed])
+        elementRef.current?.setSelectionRange(input.cursorPos, input.cursorPos)
+    }, [input.changed])
 
     return (
         <StyledTextInput
-            ref={ref}
-            $invalid={inp.isInvalid}
+            ref={elementRef}
+            $invalid={input.invalid}
             type="text"
-            value={inp.value}
-            onBlur={ev => { if (inp.onBlur) { inp.onBlur(ev, inp) } } }
-            onChange={ev => {
-                const { pos, value } = inp.filter((ev.target.selectionStart as number), ev.target.value)
-                inp.setValue(value)
-                inp.setCursorPos(pos)
-                toggleChange()
+            value={input.value}
+            onBlur={input.onblur}
+            onChange={e => {
+                input.toggleChanged()
+                var [value, cursorPos]: [string, number | undefined] = [e.target.value, e.target.selectionStart || 0]
+                if (input.oninput) var { value, cursorPos } = input.oninput(value, cursorPos, e)
+                input.setValue(value)
+                input.setCursorPos(cursorPos || value.length)
             }}
         />
     )
 })
 
-export const BTCamountTextInputView = ({ inp }: IFiltredTextInputProps) => {
-    inp.onBlur = (ev, inp) => inp.setValue(String(Number(inp.value)))
-    inp.filter = (pos, raw) => {
-        let value = ''
+interface PropsWithOnlyFilteredTextInputRef {
+    inputRef?: FilteredTextInputRef
+}
 
-        for (let char of raw) {
-            if ('0123456789'.includes(char)) {
-                value += char
-            }
-            else if ('.,'.includes(char) && !value.includes('.')) {
-                value += '.'
-            }
-            else {
-                --pos
-            }
-        }
+export const BitcoinAmountTextInputView = ({ inputRef }: PropsWithOnlyFilteredTextInputRef) => {
+    return <FilteredTextInputView
+        oninput={(value, cursorPos) => {
+            if (!value) return { value, cursorPos }
+            const index = cursorPos - 1
+            const char = value[index]
+            var skip = true
 
-        return { pos, value }
-    }
-    return <FiltredTextInputView inp={inp} />
+            if (char == '.' || char == ',') skip = !isSingleChar(value, index)
+            else if (isDigitChar(char)) skip = false
+
+            if (skip) {
+                value = removeChar(value, index)
+                cursorPos = index
+            }
+            return { value, cursorPos }
+        }}
+        onblur={e => inputRef?.current?.setValue(String(Number(inputRef.current.value)))}
+        defvalue="0"
+        inputRef={inputRef}
+    />
 }
